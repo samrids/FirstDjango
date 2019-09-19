121686e9190235da82c4c17a8210f78133cf47ba
Samrid Somboon <samrids@gmail.com>
2019-09-19 20:36:35 +0700

    Add List_perpage, list_filter, search_fields, fieldsets,

--- pukshop/admin.py
+++ pukshop/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Product
 
+
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'price', 'image_tag')
+    list_display = ['title', 'description', 'price', 'for_sale', 'image_tag']
+    list_per_page = 10
+    list_filter = ['title', 'for_sale', 'release_date']
+    search_fields = ['title', 'description']
+
+    fieldsets = [
+        (None, {'fields': ['title', 'description']}),
+        ("Sale Info", {'fields': ['price', 'for_sale'], 'classes': ['expand']}),
+        ("Image Info", {'fields': ['image']})
+    ]
+
 
 admin.site.register(Product, ProductAdmin)
--- pukshop/models.py
+++ pukshop/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 from django.utils.safestring import mark_safe
 
+
 # Create your models here.
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(decimal_places = 2, max_digits = 20, default = 00.00)
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=00.00)
     image = models.ImageField(upload_to="gallery", null=True, blank=True)
+    for_sale = models.BooleanField(null=False, default=False)
+    release_date = models.DateField(null=False, auto_now_add=True)
+    sys_created_date = models.DateTimeField(auto_now_add=True, null=False)
 
     def image_tag(self):
         if self.image:
             return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image.url)
         else:
             return 'No Image Found'
-    image_tag.short_description = 'Image'
 
+    image_tag.short_description = 'Image'
 
     def __str__(self):
         return self.title
